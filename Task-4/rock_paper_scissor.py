10d85bdc0a829c625e5b3029bb4a0b78c3d9f5bd
afzal-10-02 <afzalj_jamal@srmus.edu.in>
2024-09-13 21:54:37 +0530

    your update added

--- Task-4/rock_paper_scissor.py
+++ Task-4/rock_paper_scissor.py
@@ -1,29 +1,47 @@
+from msilib.schema import Font
 import pygame
 import sys
 import random 
 
+
 def main():
+    global Your_score
+    Your_score = 0
+    global Computer_score
+    Computer_score = 0
+
     pygame.init()
 
     white = (255, 255, 255)
     black = (0, 0, 0)
 
-    global font
-    font = pygame.font.SysFont(None, 16)
+    #font Style
+    global font, Font
+    font = pygame.font.SysFont('Arial', 20 , bold= True, italic= True)
+    Font = pygame.font.SysFont('Georgia', size= 22, bold=True)
 
     global screen
     screen = pygame.display.set_mode((500, 600))
     pygame.display.set_caption('Rock Papper Scissor')
-    clock = pygame.time.Clock()
 
-    rock = pygame.image.load("Images\img2.png")
-    paper = pygame.image.load("Images\img1.png")
-    scissor = pygame.image.load("Images\img3.png")
+    #Texts
+    text = font.render("Pick your Move", True, (0,0,0))
+    text_rect = text.get_rect()
+    text_rect.center = (250, 325)
 
+
+    #Images
+    rock = pygame.image.load("Images/img2.png")
+    paper = pygame.image.load("Images/img1.png")
+    scissor = pygame.image.load("Images/img3.png")
+
+    #Resized images.
     rock_r = pygame.transform.scale(rock, (130,130))
     paper_r = pygame.transform.scale(paper, (130,130))
     scissor_r = pygame.transform.scale(scissor, (130,130))
 
+
+    #Button for the reset
     reset_button = pygame.Rect(0, 515, 90, 30) 
     reset_button.centerx = 500//2
 
@@ -40,20 +58,31 @@ def main():
                 x, y = event.pos
                 if 20<= x <=150 and 350 <= y <= 480:
                     user_choice = 'rock'
-                if 20+130+35 <= x <= 20+130+35+130 and 350 <= y <= 350+130:
+                elif 20+130+35 <= x <= 20+130+35+130 and 350 <= y <= 350+130:
                     user_choice = 'paper'
-                if 20+130+35+130+35 <= x <= 20+130+35+130+35+130 and 350 <= y <= 350+130:
+                elif 20+130+35+130+35 <= x <= 20+130+35+130+35+130 and 350 <= y <= 350+130:
                     user_choice = 'scissor'
-                game_winner = winner(user_choice)
-                print(game_winner)
+                else:
+                    continue
+
+                game_winner, com_choice = winner(user_choice)
+
+                if game_winner == 'Computer Win':
+                    Computer_score += 1
+                elif game_winner == 'You Win':
+                    Your_score += 1
+                print(f'{game_winner},{user_choice}')
 
                 if game_winner:
-                    display_message("User_Win" , (20,20))
+                    print(com_choice)
                 elif not game_winner:
-                    display_message("Computer  Win" , (20,20))
+                    print(com_choice)
 
+            score_text , score_text_rect = update_score(game_winner)
 
             screen.fill(white)
+            screen.blit(score_text, score_text_rect)
+            screen.blit(text, text_rect)
             screen.blit(rock_r, (20,350))
             screen.blit(paper_r, (185,350))
             screen.blit(scissor_r, (350,350))
@@ -63,23 +92,40 @@ def main():
         pygame.display.flip()
 
 
+def update_score(Winner):
+    #Variable to Score the Score.
+    global Computer_score
+    global Your_score
+
+    if winner == "Computer Win":
+        Computer_score += 1
+    elif winner == "You Win":
+        Your_score += 1
+
+    #text 
+    score_text = Font.render(f"You : {Your_score}    {Computer_score} : Computer",True , (0,0,0))
+    score_text_rect = score_text.get_rect()
+    score_text_rect.center = (250, 50)
+    return score_text, score_text_rect
 
 
 def winner(user_choice):
     l = ['rock', 'paper', 'scissor']
     com_choice = random.choice(l)
     if user_choice == 'rock' and com_choice =='scissor':
-        return 1
+        return "You Win",com_choice
     elif user_choice == 'paper' and com_choice =='rock':
-        return 1
+        return 'You Win', com_choice
     elif user_choice == 'scissor' and com_choice =='paper':
-        return 1
+        return 'You Win', com_choice
+    elif user_choice == com_choice:
+        return 'Draw',com_choice
     else:
-        return 0
+        return 'Computer Win', com_choice
 
 
 def display_message(message, pos):
-    text = font.render(message, True, (1,34,56))
+    text = Font.render(message, True, (0,0,0))
     screen.blit(text, pos)
 
 
